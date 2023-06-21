83f52d92e6f00bfc98f976391b0fc973ffed0679
uknow-mac <yoon6763@naver.com>
2023-06-21 16:15:52 +0900

    observer pattern

---
+++ src/observer/Customer.java
@@ -0,0 +1,14 @@
+package observer;
+
+public class Customer {
+
+    Observable observable;
+
+    public Customer(Observable observable) {
+        this.observable = observable;
+    }
+
+    public void sendMail(String message) {
+        observable.receiveMail(message);
+    }
+}
---
+++ src/observer/Main.java
@@ -0,0 +1,10 @@
+package observer;
+
+public class Main {
+    public static void main(String[] args) {
+        Company company = new Company();
+        Customer customer = new Customer(company);
+
+        customer.sendMail("안녕하세요");
+    }
+}
