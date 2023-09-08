d357acfe960f686998d960712634a12c951b480d
uknow-home <yoon6763@naver.com>
2023-09-08 23:13:32 +0900

    옵저버 패턴 링크 추가

--- src/main/java/uknowlab/designpattern/observer/exam1/Main.java
+++ src/main/java/uknowlab/designpattern/observer/exam1/Main.java
@@ -4,25 +4,15 @@ public class Main {
     public static void main(String[] args) {
         ObservableInt observableInt = new ObservableInt(0);
 
-        Person person1 = new Person("철수");
-        Person person2 = new Person("영희");
-        Person person3 = new Person("민수");
-
-        observableInt.addSubscriber(person1);
-        observableInt.addSubscriber(person2);
-        observableInt.addSubscriber(person3);
-
-        observableInt.setValue(10);
-        observableInt.setValue(20);
-        observableInt.setValue(30);
-
         observableInt.addSubscriber(new Subscriber() {
             @Override
             public void notifyUpdated(int value) {
-                System.out.println("업데이트 되었습니다!" + value);
+                System.out.println("익명 클래스 : 업데이트 된 값을 받았어요 -> " + value);
             }
         });
 
-        observableInt.setValue(40);
+        observableInt.setValue(1000);
+        observableInt.setValue(2000);
+        observableInt.setValue(3000);
     }
 }
---
+++ src/main/java/uknowlab/designpattern/strategy/Main.kt
@@ -0,0 +1,8 @@
+package uknowlab.designpattern.strategy
+
+fun main() {
+    val character = Character()
+    character.setWeapon {
+        println("검 무기 공격!")
+    }
+}
